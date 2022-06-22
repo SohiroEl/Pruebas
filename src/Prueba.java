8860d02f9746db244679dece5560d769129fd7e4
SohiroEl <juan.carballeda.carrasco@gmail.com>
2022-06-22 10:45:56 +0200

    Primero

--- src/Prueba.java
+++ src/Prueba.java
@@ -2,7 +2,8 @@
 public class Prueba {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundoss");
+		System.out.println("Hola Mundos");
 		System.out.println("Hola Mundo");
 	}
 
